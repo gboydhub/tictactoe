2a93d374de7aa649eb46873f4faf02521422ac97
Gary Boyd <eponick@gmail.com>
2018-07-30 22:26:05 -0400

    test GameBoard init/fail

--- test_tictactoe.rb
+++ test_tictactoe.rb
@@ -5,4 +5,8 @@ class TestTicTacToe < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
+
+    def test_create_game_board
+        test_board = GameBoard.new
+    end
 end
