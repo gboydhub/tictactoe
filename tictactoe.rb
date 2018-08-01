a0dacf877243053ba098173b68276a5c5d324a9a
Gary Boyd <eponick@gmail.com>
2018-08-01 14:45:11 -0400

    test seqplayer picks bottom/pass

--- tictactoe.rb
+++ tictactoe.rb
@@ -156,6 +156,8 @@ class SequentialPlayer < BasePlayer
                         find_piece = [f_x+1, f_y]
                     elsif @board.get_tile(f_x, f_y - 1) == 0
                         find_piece = [f_x, f_y-1]
+                    elsif @board.get_tile(f_x, f_y + 1) == 0
+                        find_piece = [f_x, f_y+1]
                     end
                 end
                 f_y += 1
