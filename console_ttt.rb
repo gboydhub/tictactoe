d15bed4d87483c819f6fb0f5e7481a3231750c6d
Gary Boyd <eponick@gmail.com>
2018-08-01 12:01:44 -0400

    Added PvP and PvC[random] to console frontend

--- console_ttt.rb
+++ console_ttt.rb
@@ -1,6 +1,10 @@
 require_relative 'tictactoe.rb'
 
+avail_pieces = ['O', 'X']
+
 def display_board(board)
+    puts " |1 2 3"
+    puts "-------"
     for y in 0..board.height-1
         for x in 0..board.width-1
             draw_piece = " "
@@ -11,27 +15,108 @@ def display_board(board)
             if x == board.width-1
                 sep = "\n"
             end
+            if x == 0
+                print "#{('A'.ord+y).chr}|"
+            end
             print draw_piece + sep
         end
     end
 end
 
-board = GameBoard.new
-random_player = RandomPlayer.new
-random_playerB = RandomPlayer.new
-random_player.set_board(board)
-random_player.piece = :X
-random_playerB.set_board(board)
-random_playerB.piece = :O
+def ask_player_move(board, piece)
+    pick_move = ""
+    valid_move = false
+    x_loc = 0
+    y_loc = 0
+    while valid_move == false do
+        print "\n#{piece}, it is your turn.\nChoose a location [ex B3]:"
+        move_picked = gets.chomp
+        if move_picked.length == 2
+            y_loc = move_picked[0]
+            x_loc = move_picked[1]
 
-while !board.check_winner() do
-    system 'cls'
-    if board.turns_taken % 2 == 0
-        random_player.take_turn()
-    else
-        random_playerB.take_turn()
+            if x_loc.to_i.to_s == x_loc #X loc is a number
+                if x_loc.to_i <= board.width #X is valid
+                    x_loc = x_loc.to_i - 1
+                    y_loc = (y_loc.ord - "A".ord)
+                    if y_loc < board.height #Y is valid
+                        if board.set_tile(x_loc, y_loc, piece) #Move is valid on board
+                            valid_move = true
+                        else
+                            puts "That is an invalid move, try another."
+                        end
+                    end
+                end
+            end
+        end
     end
-    display_board(board)
 end
 
-puts "#{board.check_winner()} wins the game!"
+
+num_humans = -1
+while num_humans < 0 || num_humans > 2
+    print "How many human players [0-2]: "
+    num_humans = gets.chomp.to_i
+end
+
+choose_piece = "-"
+if num_humans > 0
+    while !avail_pieces.include?(choose_piece) do
+        print "Player 1, do you want to be O or X (blank for random): "
+        choose_piece = gets.chomp.upcase
+        if choose_piece == ""
+            choose_piece = avail_pieces[Random.rand(avail_pieces.length)]
+        end
+    end
+    avail_pieces.delete_at(avail_pieces.index(choose_piece))
+end
+player_piece = [choose_piece]
+if num_humans == 2
+    player_piece << avail_pieces[0]
+end
+
+#Create required AI's
+ai_list = []
+ai_gen_counter = 2
+while ai_gen_counter > num_humans
+    ai_list << RandomPlayer.new
+    ai_gen_counter -= 1
+end
+
+#Setup board and AI
+board = GameBoard.new
+ai_list.each_with_index do |val, ind|
+    ai_list[ind].set_board(board)
+    ai_list[ind].piece = avail_pieces[ind]
+end
+
+#Game loop
+while !board.check_winner() do
+    system 'cls'
+    display_board(board)
+    if num_humans == 0
+        ai_list[board.turns_taken % 2].take_turn()
+    elsif num_humans == 1
+        if board.turns_taken % 2 == 0
+            ask_player_move(board, player_piece[0])
+        else
+            ai_list[0].take_turn()
+        end
+    else
+        if board.turns_taken % 2 == 0
+            ask_player_move(board, :O)
+        else
+            ask_player_move(board, :X)
+        end
+    end
+end
+
+system 'cls'
+display_board(board)
+#Display winner
+winner = board.check_winner()
+if winner == "Draw"
+    puts "\nThe game is a draw!"
+else
+    puts "\n#{winner} wins the game!"
+end
