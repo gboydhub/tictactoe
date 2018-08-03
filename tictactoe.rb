027d52828a9cc7b94081ecdec0c39d362ff2a692
Gary Boyd <eponick@gmail.com>
2018-08-03 00:14:27 -0400

    Added final AI steps

--- tictactoe.rb
+++ tictactoe.rb
@@ -240,6 +240,47 @@ class UnbeatablePlayer < BasePlayer
                 return true
             end
         end
+
+        #Claim center
+        if @board.get_tile(1, 1) == 0
+            @board.set_tile(1, 1, @piece)
+            return true
+        end
+
+        #Claim corner
+        if @board.get_tile(0, 0) == 0
+            @board.set_tile(0, 0, @piece)
+            return true
+        if @board.get_tile(0, 2) == 0
+            @board.set_tile(0, 2, @piece)
+            return true
+        end
+        if @board.get_tile(2, 0) == 0
+            @board.set_tile(2, 0, @piece)
+            return true
+        end
+        if @board.get_tile(2, 2) == 0
+            @board.set_tile(2, 2, @piece)
+            return true
+        end
+
+        #Claim side
+        if @board.get_tile(1, 0) == 0
+            @board.set_tile(1, 0, @piece)
+            return true
+        end
+        if @board.get_tile(1, 2) == 0
+            @board.set_tile(1, 2, @piece)
+            return true
+        end
+        if @board.get_tile(0, 1) == 0
+            @board.set_tile(0, 1, @piece)
+            return true
+        end
+        if @board.get_tile(2, 1) == 0
+            @board.set_tile(2, 1, @piece)
+            return true
+        end
         false
     end
 
@@ -553,7 +594,7 @@ end
 # ~Win: If the player has two in a row, they can place a third to get three in a row.
 # ~Block: If the opponent has two in a row, the player must play the third themselves to block the opponent.
 # ~Fork: Create an opportunity where the player has two threats to win (two non-blocked lines of 2).
-# Blocking an opponent's fork: If there is only one possible fork for the opponent, the player should block it. Otherwise, the player should block any forks in any way that simultaneously allows them to create two in a row. Otherwise, the player should create a two in a row to force the opponent into defending, as long as it doesn't result in them creating a fork. For example, if "X" has two opposite corners and "O" has the center, "O" must not play a corner in order to win. (Playing a corner in this scenario creates a fork for "X" to win.)
+# ~?Blocking an opponent's fork: If there is only one possible fork for the opponent, the player should block it. Otherwise, the player should block any forks in any way that simultaneously allows them to create two in a row. Otherwise, the player should create a two in a row to force the opponent into defending, as long as it doesn't result in them creating a fork. For example, if "X" has two opposite corners and "O" has the center, "O" must not play a corner in order to win. (Playing a corner in this scenario creates a fork for "X" to win.)
 
 # Center: A player marks the center. (If it is the first move of the game, playing on a corner gives the second player more opportunities to make a mistake and may therefore be the better choice; however, it makes no difference between perfect players.)
 
