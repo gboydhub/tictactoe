4e3b52868162857e00c58e5f719671a070c537d3
Gary Boyd <eponick@gmail.com>
2018-08-05 15:12:25 -0400

    Adding new game backend

--- app.rb
+++ app.rb
@@ -29,28 +29,69 @@ post '/turn' do
   end
 end
 
+post '/new_game' do
+  session[:gameobj] = GameBoard.new
+  session[:player1] = "Human"
+  session[:player2] = "Human" #Default players to human to catch wonky form data
+
+  if params[:p1_type] == "ai" #X is pirate
+    case params[:comp1_difficulty]
+    when "1"
+      session[:player1] = RandomPlayer.new
+    when "2"
+      session[:player1] = SequentialPlayer.new
+    when "3"
+      session[:player1] = UnbeatablePlayer.new
+    end
+    session[:player1].piece = "X"
+    session[:player1].enemy_piece = "O"
+    session[:player1].set_board(session[:gameobj])
+  end
+  
+  if params[:p2_type] == "ai" #O is ninja
+    case params[:comp2_difficulty]
+    when "1"
+      session[:player2] = RandomPlayer.new
+    when "2"
+      session[:player2] = SequentialPlayer.new
+    when "3"
+      session[:player2] = UnbeatablePlayer.new
+    end
+    session[:player2].piece = "O"
+    session[:player2].enemy_piece = "X"
+    session[:player2].set_board(session[:gameobj])
+  end
+
+
+end
+
 get '/next_move' do
   if !session[:gameobj]
     redirect '/'
   end
 
   game_board = session[:gameobj]
-  if game_board.turns_taken % 2 == 1 #X's turn
-    p2 = UnbeatablePlayer.new
-    p2.piece = "X"
-    p2.enemy_piece = "O"
-    p2.set_board(game_board)
-    p2.take_turn()
-    session[:gameobj] = game_board
-    if game_board.check_winner()
-      redirect '/end_game'
+  if game_board.turns_taken % 2 == 0 #Ninja's go first (Duh)
+    if session[:player2] != "Human"
+      session[:player2].take_turn()
     end
+  else
+    if session[:player1] != "Human"
+      session[:player1].take_turn()
+    end
+  end
+  if session[:gameobj].check_winner()
+    redirect '/end_game'
   end
 
   erb :next_move, locals: {game_inst: session[:gameobj]}
 end
 
 get '/end_game' do
+  if !session[:gameobj]
+    redirect '/'
+  end
+  
   cur_board = session[:gameobj]
   winner = cur_board.check_winner();
   erb :end_game, locals: {game_inst: session[:gameobj], victor: winner}, trim: '-'
